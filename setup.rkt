982491b86adf21dbe8006704d359f5bf72858357
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-12-10 12:59:12 -0800

    spacemacs update, primarily

--- setup.rkt
+++ setup.rkt
@@ -2,16 +2,18 @@
 
 (require "lib.rkt")
 
-(p (rd "config" #:to ".config"))
+; (p (rd "config" #:to ".config"))
 
+#|
 (p (d "local" #:to ".local"
       (d "share"
          (rd "applications"))))
+|#
 
-(p (l "bin"))
+; (p (l "bin"))
 
 (p (rd "dotfiles" #:to "~" #:prefix "."))
 
-(p (rd "xournal" #:to ".xournal"))
+; (p (rd "xournal" #:to ".xournal"))
 
 (p (rd "spacemacs-private" #:to ".emacs.d/private"))
